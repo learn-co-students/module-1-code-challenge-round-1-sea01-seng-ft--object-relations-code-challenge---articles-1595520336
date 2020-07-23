71f42292f95dc58c4f4224b0a414a09c72f25c65
hellonathanchung <chung.nathanh@gmail.com>
2020-07-23 11:00:02 -0700

    nathan-chung-attempt-2

--- tools/console.rb
+++ tools/console.rb
@@ -6,9 +6,23 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+nathan = Author.new("Nathan")
+ahmed = Author.new("Ahmed")
+brad = Author.new("Brad")
+
+mag1 = Magazine.new("Time", "News")
+mag2 = Magazine.new("Shonen Jump", "Comics" )
+mag3 = Magazine.new("NY Times", "Media" )
+mag4 = Magazine.new("Sports Illustrated", "Sports" )
 
 
-
+art1 = Article.new(nathan, mag1, "how to do this")
+art5 = Article.new(nathan, mag1, "not how to do this")
+art8 = Article.new(nathan, mag1, "crazy new methods")
+art2 = Article.new(ahmed, mag1, "rocking the code")
+art2 = Article.new(ahmed, mag2, "eating chips")
+art3 = Article.new(brad, mag4, "throwing the football")
+art4 = Article.new(nathan, mag3, "writing a story")
 
 
 
