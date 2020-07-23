20094300560091f47c9b31247e7b154e33bbf38f
LucasLeibs <dr.lucasleibs@gmail.com>
2020-07-23 10:15:02 -0700

    code challenge attempt 2

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,35 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def articles
+    Article.all.select {|article| article.magazine == self}
+  end
+
+  def contributors
+    articles.map {|article| article.author}
+  end
+
+  def self.find_by_name(name)
+    Magazine.all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    articles.map {|article| article.title}
+  end
+
+  def contributing_authors
+    contributors.select {|contributor| contributors.count(contributor) > 2}.uniq
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,10 +7,19 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("Lucas")
+author2 = Author.new("Gabi")
 
+magazine1 = Magazine.new("Time","news")
+magazine2 = Magazine.new("People","drama")
+magazine3 = Magazine.new("NAT GEO","nature")
 
-
-
+article1 = Article.new(author1, magazine3, "article 1")
+article2= Article.new(author2, magazine1, "article 2")
+article3 = Article.new(author1, magazine2, "article 3")
+article4 = Article.new(author2, magazine2, "article 4")
+article5 = Article.new(author1, magazine3, "article 5")
+article6 = Article.new(author1, magazine3, "article 6")
 
 
 
@@ -18,4 +27,4 @@ end
 ### DO NOT REMOVE THIS
 binding.pry
 
-0
+
