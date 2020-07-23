9ae282319b0438b5d9b685c6b448bd780e1c3710
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-07-23 11:39:36 -0700

     Code chellange - Noa Rabin-Cohen

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,24 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
-  
   end
 
+  def articles
+    Article.all.select {|article| article.author == self}
+  end
 
+  def magazines
+    self.articles.collect {|article| article.magazine}.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    self.articles.collect {|article| article.magazine.category}.uniq
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,37 @@
 class Magazine
   attr_accessor :name, :category
-
+  
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def magazine_objects
+    Article.all.select {|article| article.magazine == self}
+  end
+
+  def contributors
+    magazine_objects.collect {|article| article.author}
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    magazine_objects.collect {|article| article.title}
+  end
+
+  def contributing_authors
+ 
   end
 
 
